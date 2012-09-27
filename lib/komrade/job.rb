081b8cf9042ae1007cd0e7d20a7dfbd79800f24d
♠ ace hacker <ryan@heroku.com>
2012-09-27 14:46:29 -0500

    not really a class

--- lib/komrade/job.rb
+++ lib/komrade/job.rb
@@ -1,22 +1,26 @@
 require "zlib"
+require "json"
 require "sequel"
 
+require "komrade/conf"
+
 module Komrade
-  class Job
+  module Job
+    extend self
 
     DB = Sequel.connect(Conf.database_url)
 
-    def self.entity(user_id, name)
+    def entity(user_id, name)
       Zlib.crc32([user_id, name].join)
     end
 
-    def self.put(user_id, name, payload)
+    def put(user_id, name, payload)
       d = {entity: entity(user_id, name), payload: JSON.dump(payload))}
       r = DB[:jobs].returning(:resource_id).insert(d)
       {"job-id" => r[0][:resource_id]}
     end
 
-    def self.get(user_id, name)
+    def get(user_id, name)
       DB[:jobs].where(entity: entity(user_id, name)).map do |r|
         {"job-id" => r[:resource_id], "payload" => r[:payload]}
       end
