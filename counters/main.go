70c57b030467f36cc45325fdf7b45e8dbac7500a
♠ ace hacker <ryan@heroku.com>
2013-01-27 10:56:40 -0800

    reset min, hour, day

--- counters/main.go
+++ counters/main.go
@@ -7,19 +7,24 @@ import (
 )
 
 func main() {
-	go resetMinute()
-	select {}
-}
-
-func resetMinute() {
-	for t := range time.Tick(time.Minute) {
+	for t := range time.Tick(time.Millisecond * 500) {
 		pg, err := sql.Open("postgres", pgurl)
 		if err != nil {
 			fmt.Printf("at=error error=%s\n", err)
 			return
 		}
-		pg.Exec("select reset_minute_counters()")
-		fmt.Printf("at=reset bucket=minute time=%s\n", t)
+		if t.Second() == 0 {
+			pg.Exec("select reset_minute_counters()")
+			fmt.Printf("at=reset bucket=minute time=%s\n", t)
+		}
+		if t.Minute() == 0 {
+			pg.Exec("select reset_hour_counters()")
+			fmt.Printf("at=reset bucket=hour time=%s\n", t)
+		}
+		if t.Hour() == 0 {
+			pg.Exec("select reset_day_counters()")
+			fmt.Printf("at=reset bucket=day time=%s\n", t)
+		}
 		pg.Close()
 	}
 }
