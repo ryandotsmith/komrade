8be23db5e92db51f81903cbcd21d0346491f9255
♠ ace hacker <ryan@heroku.com>
2013-01-27 11:01:43 -0800

    don't get carried away with resets

--- counters/main.go
+++ counters/main.go
@@ -13,17 +13,20 @@ func main() {
 			fmt.Printf("at=error error=%s\n", err)
 			return
 		}
+
 		if t.Second() == 0 {
 			pg.Exec("select reset_minute_counters()")
 			fmt.Printf("at=reset bucket=minute time=%s\n", t)
-		}
-		if t.Minute() == 0 {
-			pg.Exec("select reset_hour_counters()")
-			fmt.Printf("at=reset bucket=hour time=%s\n", t)
-		}
-		if t.Hour() == 0 {
-			pg.Exec("select reset_day_counters()")
-			fmt.Printf("at=reset bucket=day time=%s\n", t)
+
+			if t.Minute() == 0 {
+				pg.Exec("select reset_hour_counters()")
+				fmt.Printf("at=reset bucket=hour time=%s\n", t)
+
+				if t.Hour() == 0 {
+					pg.Exec("select reset_day_counters()")
+					fmt.Printf("at=reset bucket=day time=%s\n", t)
+				}
+			}
 		}
 		pg.Close()
 	}
