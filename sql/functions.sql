fae5f2fe74218ecfe375032db4d3c49b00a22800
♠ ace hacker <ryan@heroku.com>
2013-03-03 10:26:13 -0800

    new aggregates and removing old functions

--- sql/functions.sql
+++ sql/functions.sql
@@ -21,23 +21,3 @@ where
 	)
 RETURNING id, created_at, locked_at, payload
 $$ language sql;
-
-drop function if exists summarize_failed(uuid, text);
--- token, resolution (in minutes), min time, max time
-create function summarize_failed(uuid, text)
-returns table(
-	count bigint,
-	payload text
-)
-as $$
-select
-  count(*) as count,
-  payload::text
-from
-  failed_jobs
-where
-  queue = $1
-  and date_trunc($2, created_at) = date_trunc($2, now())
-group by payload::text
-order by count
-$$ language sql;
